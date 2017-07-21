0413f187c548aa5a50c34b4f6645bd36d1d2b76a
Josef Pihrt <josef.pihrt@outlook.com>
2017-07-21 21:22:21 +0200

    Refactor analyzer UseAutoProperty. Refactor refactorings ReplaceMethodWithProperty and ReplacePropertyWithMethod.

---
+++ source/Core/DocumentNodeInfo.cs
@@ -0,0 +1,26 @@
+﻿// Copyright (c) Josef Pihrt. All rights reserved. Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System.Collections.Immutable;
+using Microsoft.CodeAnalysis;
+
+namespace Roslynator
+{
+    internal struct DocumentNodeInfo
+    {
+        public DocumentNodeInfo(Document document, SyntaxNode root, ImmutableArray<SyntaxNode> nodes)
+        {
+            Document = document;
+            Root = root;
+            Nodes = nodes;
+        }
+
+        public Document Document { get; }
+        public SyntaxNode Root { get; }
+        public ImmutableArray<SyntaxNode> Nodes { get; }
+
+        public SyntaxTree SyntaxTree
+        {
+            get { return (!Nodes.IsDefaultOrEmpty) ? Nodes[0].SyntaxTree : null; }
+        }
+    }
+}
---
+++ source/Core/ReferenceFinder.cs
@@ -0,0 +1,81 @@
+﻿// Copyright (c) Josef Pihrt. All rights reserved. Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.FindSymbols;
+
+namespace Roslynator
+{
+    internal static class ReferenceFinder
+    {
+        public static async Task<ImmutableArray<DocumentNodeInfo>> FindReferences(
+            ISymbol symbol,
+            Solution solution,
+            bool allowCandidate = false,
+            CancellationToken cancellationToken = default(CancellationToken))
+        {
+            if (symbol == null)
+                throw new ArgumentNullException(nameof(symbol));
+
+            if (solution == null)
+                throw new ArgumentNullException(nameof(solution));
+
+            List<DocumentNodeInfo> infos = null;
+
+            foreach (ReferencedSymbol referencedSymbol in await SymbolFinder.FindReferencesAsync(
+                symbol,
+                solution,
+                cancellationToken).ConfigureAwait(false))
+            {
+                foreach (IGrouping<Document, ReferenceLocation> grouping in referencedSymbol.Locations.GroupBy(f => f.Document))
+                {
+                    Document document = grouping.Key;
+
+                    List<SyntaxNode> nodes = null;
+
+                    SyntaxNode root = await document.GetSyntaxRootAsync(cancellationToken).ConfigureAwait(false);
+
+                    foreach (ReferenceLocation referenceLocation in grouping)
+                    {
+                        if (!referenceLocation.IsImplicit
+                            && (allowCandidate || !referenceLocation.IsCandidateLocation))
+                        {
+                            Location location = referenceLocation.Location;
+
+                            if (location.IsInSource)
+                            {
+                                SyntaxNode node = root.FindNode(location.SourceSpan, findInsideTrivia: true, getInnermostNodeForTie: true);
+
+                                Debug.Assert(node != null);
+
+                                (nodes ?? (nodes = new List<SyntaxNode>())).Add(node);
+                            }
+                        }
+                    }
+
+                    if (nodes != null)
+                    {
+                        var info = new DocumentNodeInfo(document, root, nodes.ToImmutableArray());
+
+                        (infos ?? (infos = new List<DocumentNodeInfo>())).Add(info);
+                    }
+                }
+            }
+
+            if (infos != null)
+            {
+                return ImmutableArray.CreateRange(infos);
+            }
+            else
+            {
+                return ImmutableArray<DocumentNodeInfo>.Empty;
+            }
+        }
+    }
+}
